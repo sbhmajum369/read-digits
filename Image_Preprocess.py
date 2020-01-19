b1f6a686d2af3f655508eca39d1f57cd1a35d717
Code-Create-ai <60013624+Code-Create-ai@users.noreply.github.com>
2020-01-19 15:14:06 -0500

    Update Image_Preprocess.py

--- Image_Preprocess.py
+++ Image_Preprocess.py
@@ -4,24 +4,25 @@ import cv2
 import numpy as np
 from numpy import asarray
 from numpy import save
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 import os
 
-os.chdir('D:/Fall 2019/Machine and Deep Learning/Project/New _Dataset/Train/')	# Home directory of images
+os.chdir('D:/Images')	# Home directory of images
 
 n=int(input("Enter num of images for Training and Val:"))
 
-folder = 'D:/Fall 2019/Machine and Deep Learning/Project/New _Dataset/Testing'	# New directory for storing resized images
+folder = 'D:/Resized_Images'	# New directory for storing resized images
 
 for img in range(1,n+1):
 	img_data=cv2.imread(str(img)+'.png')
-	new_img=cv2.resize(img_data, (32,64), interpolation = cv2.INTER_AREA)
+	new_img=cv2.resize(img_data, (32,64), interpolation = cv2.INTER_AREA)	# Resizing in 64x32 format
 	cv2.imwrite(os.path.join(folder , str(img)+'.png'), new_img)
 
-# print("....Resizing Complete....")
+print("....Resizing Complete....")
 os.chdir(folder)
 
+# Storing in a numpy format
 photos= list()
 # Enumerate files in the directory
 for file in range(1,n+1):
